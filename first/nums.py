60cd629accd69c351c37f46d5398f977da0d0bbb
evggor87 <evggor87@gmail.com>
2022-04-21 22:19:58 +0300

    add module strings.py, delete text

--- first/nums.py
+++ first/nums.py
@@ -2,13 +2,15 @@ def plus(a, b):
     return a + b
 
 
-text = "new text"
-
-
 def minus(a, b):
     return a - b
+
+
+def mul(a, b):
+    return a * b
 
 
 if __name__ == '__main__':
     print(plus(2, 3))
     print(minus(4, 3))
+    print(mul(4, 3))
